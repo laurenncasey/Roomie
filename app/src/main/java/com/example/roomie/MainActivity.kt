3a15c7f25508668dc55ac33620d4d50abf2bd159
Blake Furlano <furlanbg9025@uwec.edu>
2021-12-09 11:37:24 -0600

    Update issues with branch merges

--- app/src/main/java/com/example/roomie/MainActivity.kt
+++ app/src/main/java/com/example/roomie/MainActivity.kt
@@ -6,9 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.room.Room
-import androidx.fragment.app.Fragment
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
 
 /**
  * Author: Lauren Casey
@@ -17,23 +14,31 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //verify with database here
+
         val signUpB = findViewById<Button>(R.id.signup)
         val logInB = findViewById<Button>(R.id.login)
         val forgotPwdB = findViewById<Button>(R.id.forgotpwd)
+
         val usrInput= findViewById<TextView>(R.id.usrname)
         val pwdInput = findViewById<TextView>(R.id.pwd)
-
+        val db = Room.databaseBuilder(
+            applicationContext,
+            RoomieDB::class.java, "database-name"
+        ).build()
+        val userDao = db.userDao()
 
        findViewById<Button>(R.id.signup).setOnClickListener{
            startActivity(Intent(this, SignUp::class.java))
        }
         findViewById<Button>(R.id.login).setOnClickListener{
-
-            //search DB
             startActivity(Intent(this, Profile::class.java))
         }
 
@@ -42,4 +47,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
